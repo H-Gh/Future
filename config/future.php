6b9b1086727dafdae76017d53e42a642d288be83
Hamed Ghasempour <business.hamedghasempour@gmail.com>
2021-02-24 06:14:48 +0400

    Add config for the holding basic information:

---
+++ config/future.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "seconds_per_day" => 86400,
+    "seconds_per_sol" => 88775.244147,
+    "leap_seconds" => 37,
+    "msd_precision" => 5,
+    "correction" => 34127.2954262,
+];
